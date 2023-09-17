8dbe545df8c0f717466df10900afb808fed70551
AdelAlAtram <adelaaymann@gmail.com>
2023-09-17 20:14:08 +0300

    ticket 1 has been solved

--- controllers/roles.js
+++ controllers/roles.js
@@ -1,18 +1,85 @@
+const {pool} = require("../models/db")
+
+
+
+// const funcName=(req,res)=>{
+
+   
+// try {
+//   const { name, email, password } = req.body;
+
+//   const query = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING email, name, id`;
+//   const values = [name, email, password];
+//   const response = await pool.query(query, values);
+
+//   if (response.rowCount) {
+//     res.status(201).json({
+//       success: true,
+//       message: "user created successfully",
+//       response: response.rows,
+//     });
+//   }
+// } catch (error) {
+//   console.log(error);
+//   res.status(500).json({
+//     success: false,
+//     message: "Server Error",
+//     error: error.message,
+//   });
+// }
+   
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 // This function creates new role
-const createNewRole = (req, res) => {
-  //TODO: write your code here
+const createNewRole = async(req, res) => {
+  try {
+    const {role} = req.body;
+
+    const query = `INSERT INTO roles (role) VALUES ($1) RETURNING role, id`;
+    const values = [role];
+    const response = await pool.query(query, values);
+
+    if (response.rowCount) {
+      res.status(201).json({
+        success: true,
+        message: "Role created successfully",
+        role: response.rows,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: error.message,
+    });
+  }
 };
 
 // This function creates new permission
 const createNewPermission = (req, res) => {
-  //TODO: write your code here
+  
 };
 
 // This function creates new role permission
 const createNewRolePermission = (req, res) => {
-  //TODO: write your code here
+  
 };
 
 module.exports = {
-
+  createNewRole,
+  createNewPermission,
+  createNewRolePermission
 };
