16613ecd7d3501b582d24d01089347de3d280f01
AdelAlAtram <adelaaymann@gmail.com>
2023-09-17 23:09:04 +0300

    tickets 2,3 has been solved

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,9 @@
 
-// This function creates (new user)
+const jwt = require("jsonwebtoken");
+
+// getting environment variables
+const SECRET = process.env.SECRET;
+
 const {pool} = require("../models/db")
 const bcrypt = require("bcrypt");
 
@@ -30,12 +34,56 @@ const register =async (req, res) => {
 };
 
 
+const login =async (req, res) => {
 
-const login = (req, res) => {
-   
+  try {
+    const {email,password} = req.body;
+    const query = `SELECT * FROM users WHERE email = $1;`;
+    const value = [email]
+    const response = await pool.query(query,value);
+    
+    // console.log(response);
+    if (response.rows.length) {
+      const result = await bcrypt.compare(password, response.rows[0].password);
+      if(result){
+        const payload = {
+          userId:  response.rows[0]._id,
+          country:  response.rows[0].country,
+          role:  response.rows[0].role_id,
+        };
+
+        const options = {
+          expiresIn: "60m",
+        };
+        const token = jwt.sign(payload, SECRET, options);
+        res.status(200).json({
+          success: true,
+          message: `Valid login credentials`,
+          token: token,
+        });
+      }
+
+
+
+     
+      res.status(201).json({
+        success: true,
+        message: "Valid login credentials",
+        response: response.rows[0],
+      });
+    }
+  
+  } catch (error) {
+    console.log(error);
+    res.status(409).json({
+      success: false,
+      message: "The email doesn’t exist or the password you’ve entered is incorrect",
+      error: error.message,
+    });
+  }
   };
   
 
 module.exports = {
-  register
+  register,login
 };
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const userRouter = express.Router();
 // Import users controllers
-const {register} =require("../controllers/users")
+const {register, login} =require("../controllers/users")
 userRouter.post("/register",register)
+userRouter.post("/login",login)
 
 
 module.exports = userRouter;
